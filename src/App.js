9a07b1980878672e19ba296393b1fc39a263353a
Paige Niedringhaus <paigen11@gmail.com>
2019-09-20 17:20:32 -0400

    adding in breakpoint stuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { LastLocationProvider } from 'react-router-last-location';
+import { BreakpointProvider } from 'react-socks';
 import NowPlaying from './containers/NowPlaying/NowPlaying';
 import Dashboard from './containers/Dashboard/Dashboard';
 import Upcoming from './containers/Upcoming/Upcoming';
@@ -13,26 +14,28 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <LastLocationProvider>
-          <Header />
-          <Switch>
-            <Route exact path="/" component={Dashboard} />
-            <Route exact path="/search" component={MovieSearch} />
-            <Route exact path="/nowplaying" component={NowPlaying} />
-            <Route exact path="/upcoming" component={Upcoming} />
-            <Route exact path="/genres" component={Genres} />
-            <Route
-              exact
-              path="/genres/:genreName/:genreId"
-              component={GenreList}
-            />
-            <Route exact path="/movie/:id" component={MovieDetails} />
-          </Switch>
-        </LastLocationProvider>
-      </Router>
-    </div>
+    <BreakpointProvider>
+      <div className="App">
+        <Router>
+          <LastLocationProvider>
+            <Header />
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route exact path="/search" component={MovieSearch} />
+              <Route exact path="/nowplaying" component={NowPlaying} />
+              <Route exact path="/upcoming" component={Upcoming} />
+              <Route exact path="/genres" component={Genres} />
+              <Route
+                exact
+                path="/genres/:genreName/:genreId"
+                component={GenreList}
+              />
+              <Route exact path="/movie/:id" component={MovieDetails} />
+            </Switch>
+          </LastLocationProvider>
+        </Router>
+      </div>
+    </BreakpointProvider>
   );
 }
 
