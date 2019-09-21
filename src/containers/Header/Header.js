470bbb868d60c411a89107cd0057964fb261aac9
Paige Niedringhaus <paigen11@gmail.com>
2019-09-21 13:11:27 -0400

    header with new sidebar component

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,32 +1,42 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { Breakpoint } from 'react-socks';
 import TmdbIcon from '../../assets/tmdb-power.png';
+import Sidebar from './Sidebar';
 import './Header.scss';
 
-function Header() {
+const Header = () => {
   return (
     <nav className="navbar-wrapper">
-      <ul className="navbar-links">
-        <li className="navbar-link">
-          <NavLink to="/">
-            <img src={TmdbIcon} alt="logo" />
-          </NavLink>
-        </li>
-        <li className="navbar-link">
-          <NavLink to="/nowplaying">Now Playing</NavLink>
-        </li>
-        <li className="navbar-link">
-          <NavLink to="/upcoming">Coming Soon</NavLink>
-        </li>
-        <li className="navbar-link">
-          <NavLink to="/search">Movie Search</NavLink>
-        </li>
-        <li className="navbar-link">
-          <NavLink to="/genres">Genres</NavLink>
-        </li>
-      </ul>
+      <Breakpoint medium up>
+        <ul className="navbar-links">
+          <li className="navbar-link-logo">
+            <NavLink to="/">
+              <img src={TmdbIcon} alt="logo" />
+            </NavLink>
+          </li>
+
+          <div className="navbar-top-links">
+            <li className="navbar-link">
+              <NavLink to="/nowplaying">Now Playing</NavLink>
+            </li>
+            <li className="navbar-link">
+              <NavLink to="/upcoming">Coming Soon</NavLink>
+            </li>
+            <li className="navbar-link">
+              <NavLink to="/search">Movie Search</NavLink>
+            </li>
+            <li className="navbar-link">
+              <NavLink to="/genres">Genres</NavLink>
+            </li>
+          </div>
+        </ul>
+      </Breakpoint>
+      <Breakpoint small down>
+        <Sidebar />
+      </Breakpoint>
     </nav>
   );
-}
+};
 
 export default Header;
