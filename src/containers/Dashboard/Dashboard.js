c3359ec5bbcea6a6e80fdfbcecda5d08b74508da
Paige Niedringhaus <paigen11@gmail.com>
2019-09-25 14:22:27 -0400

    fixing animation and adding tablet desktop

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -7,8 +7,36 @@ export class Dashboard extends Component {
   render() {
     return (
       <>
-        <Breakpoint medium up>
+        <Breakpoint large up>
           <div className="dashboard" data-testid="main__dashboard">
+            <div className="dashboard-card-wrapper">
+              <NavLink to="/nowplaying">
+                <div>
+                  <i className="fa fa-ticket" aria-hidden="true" />
+                  <h1>Now Playing</h1>
+                </div>
+              </NavLink>
+            </div>
+            <div className="dashboard-card-wrapper">
+              <NavLink to="/upcoming">
+                <div>
+                  <i className="fa fa-video-camera" aria-hidden="true" />
+                  <h1>Upcoming Releases</h1>
+                </div>
+              </NavLink>
+            </div>
+            <div className="dashboard-card-wrapper">
+              <NavLink to="/genres">
+                <div>
+                  <i className="fa fa-film" aria-hidden="true" />
+                  <h1>Genres</h1>
+                </div>
+              </NavLink>
+            </div>
+          </div>
+        </Breakpoint>
+        <Breakpoint medium up>
+          <div className="dashboard-tablet" data-testid="main__dashboard">
             <div className="dashboard-card-wrapper">
               <NavLink to="/nowplaying">
                 <div>
