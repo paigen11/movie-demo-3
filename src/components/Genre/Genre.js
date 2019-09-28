0e60f4e4eccf3bdfe16d4cc5278ad032333a5989
Paige Niedringhaus <paigen11@gmail.com>
2019-09-28 13:35:56 -0400

    adding genre updates

--- src/components/Genre/Genre.js
+++ src/components/Genre/Genre.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './Genre.scss';
 
 const Genre = props => (
-  <div
-    key={props.id}
-    className="genre-component"
-    onClick={() => props.goToGenreList(props.id, props.name)}
-  >
-    <h3 className="genre-name">{props.name}</h3>
-  </div>
+  <>
+    <div
+      key={props.id}
+      className="genre-component"
+      onClick={() => props.goToGenreList(props.id, props.name)}
+    >
+      <h3 className="genre-name">{props.name}</h3>
+    </div>
+  </>
 );
 
 export default Genre;
